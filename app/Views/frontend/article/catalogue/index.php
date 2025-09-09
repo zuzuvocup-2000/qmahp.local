3a0b6a7d422d5809835d635aefe8951ccd1971d7
MinhHL2NF <minhhl@2nf.com.vn>
2025-09-09 16:33:22 +0700

    update css article catalogue

--- app/Views/frontend/article/catalogue/index.php
+++ app/Views/frontend/article/catalogue/index.php
@@ -1,3 +1,83 @@
+<?php
+    $model = new App\Models\AutoloadModel();
+    $articleByCatalogue = $model->_get_where([
+        'select'   => 'tb1.id, tb2.title, tb1.image, tb2.canonical, tb2.description, tb1.created_at', // <-- thêm tb1.id
+        'where'    => [
+            'tb1.deleted_at' => 0,
+            'tb1.publish'    => 1,
+            'tb1.level'      => 2,
+            'tb1.parentid'   => $detailCatalogue['parentid'],
+        ],
+        'table'    => 'article_catalogue as tb1',
+        'join'     => [
+            [
+                'article_translate as tb2',
+                'tb2.module = "article_catalogue" AND tb2.objectid = tb1.id AND tb2.language = \''.$language.'\'',
+                'inner'
+            ]
+        ],
+        'group_by' => 'tb1.id',
+        'order_by' => 'tb1.created_at desc'
+    ], true);
+    
+    if (!empty($articleByCatalogue) && is_array($articleByCatalogue)) {
+        foreach ($articleByCatalogue as $key => $val) {
+            $catId = (int) $val['id'];
+    
+            $articleByCatalogue[$key]['post'] = $model->_get_where([
+                'select' => 'tb1.id, tb2.title, tb1.image, tb2.canonical, tb1.created_at',
+                'table'  => 'article as tb1',
+                'join'   => [
+                    [
+                        'article_translate as tb2',
+                        'tb2.objectid = tb1.id AND tb2.module = "article" AND tb2.language = \''.$language.'\'',
+                        'inner'
+                    ],
+                ],
+                'where'  => [
+                    'tb1.publish'     => 1,
+                    'tb1.deleted_at'  => 0,
+                    'tb1.catalogueid' => $catId,
+                ],
+                'order_by' => 'tb1.created_at DESC',
+                'limit' => '5',
+            ], true);
+        }
+    }
+
+    $articleListByYear = [];
+
+    $news = $model->_get_where([
+        'select' => 'tb1.id, tb2.title, tb1.image, tb2.canonical, tb1.created_at',
+        'table'  => 'article as tb1',
+        'join'   => [
+            [
+                'article_translate as tb2',
+                'tb2.objectid = tb1.id AND tb2.module = "article" AND tb2.language = \''.$language.'\'',
+                'inner'
+            ],
+        ],
+        'where'  => [
+            'tb1.publish'     => 1,
+            'tb1.deleted_at'  => 0,
+        ],
+        'order_by' => 'tb1.created_at DESC',
+    ], true);
+
+    if (!empty($news) && is_array($news)) {
+        foreach ($news as $new) {
+            $year = date('Y', strtotime($new['created_at']));
+    
+            if (!isset($articleListByYear[$year])) {
+                $articleListByYear[$year] = [];
+            }
+    
+            $articleListByYear[$year][] = $new;
+        }
+    
+        ksort($articleListByYear);
+    }
+?>
 <!-- Hero Section -->
 <section class="hero-section" style="background-image: url(<?php echo !empty($detailCatalogue['image']) ? $detailCatalogue['image'] : '/public/frontend/resources/images/hero-bg.jpg' ?>);">
     <div class="hero-overlay"></div>
@@ -29,45 +109,71 @@
                 <div class="categories-title">Danh mục</div>
                 <div class="categories-list">
                     <!-- Main Categories -->
-                    <?php if(isset($child) && is_array($child) && count($child)){ 
-                        foreach($child as $cat){ 
+                    <?php if(isset($articleByCatalogue) && is_array($articleByCatalogue) && count($articleByCatalogue)){ 
+                        foreach($articleByCatalogue as $key => $val){ 
                     ?>
                     <div class="category-item">
-                        <a href="<?php echo $cat['canonical'].HTSUFFIX ?>" class="category-link">
-                            <i class="fa fa-chevron-down"></i>
-                            <?php echo $cat['title'] ?>
-                        </a>
-                        <div class="category-date"><?php echo date('d/m/y', strtotime($cat['created_at'])) ?></div>
+                        <div class="uk-flex uk-flex-space-between">
+                            <a href="<?php echo $val['canonical'].HTSUFFIX ?>" class="category-link">
+                                <?php echo $val['title'] ?>
+                                <div class="category-date"><?php echo date('d/m/y', strtotime($val['created_at'])) ?></div>
+                            </a>
+                            <?php if(isset($val['post']) && is_array($val['post']) && count($val['post'])){ ?>
+                            <div class="btn-more" data-toggle="submenu-<?php echo $key ?>">
+                                <i class="fa fa-chevron-down"></i>
+                            </div>
+                            <?php } ?>
+                        </div>
+                        <?php if(isset($val['post']) && is_array($val['post']) && count($val['post'])){ ?>
+                        <ul class="uk-list uk-clearfix mt10 submenu" id="submenu-<?php echo $key ?>" style="display: none;">
+                            <?php foreach($val['post'] as $keyPost => $valPost){  ?>
+                            <li class="uk-flex uk-flex-space-between">
+                                <a href="<?php echo $valPost['canonical'].HTSUFFIX ?>"><?php echo $valPost['title'] ?></a>
+                                <div class="category-date"><?php echo date('d/m/y', strtotime($valPost['created_at'])) ?></div>
+                            </li>
+                            <?php } ?>
+                        </ul>
+                        <?php } ?>
                     </div>
                     <?php }} ?>
-                    
+                </div>
+                <div class="categories-title">Tin tức theo năm</div>
+                <div class="categories-list">
                     <!-- Year Categories -->
-                    <div class="year-categories">
-                        <div class="year-item">
-                            <a href="#" class="year-link">
+                    <?php if(isset($articleListByYear) && is_array($articleListByYear) && count($articleListByYear)){ 
+                        $firstYear = true;
+                        foreach($articleListByYear as $year => $articles){ 
+                    ?>
+                    <div class="category-item">
+                        <div class="uk-flex uk-flex-space-between">
+                            <a href="#" class="category-link">
+                                <?php echo $year ?>
+                                <div class="category-date"><?php echo count($articles) ?> bài viết</div>
+                            </a>
+                            <div class="btn-more" data-toggle="year-<?php echo $year ?>">
                                 <i class="fa fa-chevron-down"></i>
-                                2021
-                            </a>
+                            </div>
                         </div>
-                        <div class="year-item">
-                            <a href="#" class="year-link">
-                                <i class="fa fa-chevron-right"></i>
-                                2023
-                            </a>
-                        </div>
-                        <div class="year-item">
-                            <a href="#" class="year-link">
-                                <i class="fa fa-chevron-right"></i>
-                                2024
-                            </a>
-                        </div>
-                        <div class="year-item">
-                            <a href="#" class="year-link">
-                                <i class="fa fa-chevron-right"></i>
-                                2025
-                            </a>
+                        <ul class="uk-list uk-clearfix mt10 submenu" id="year-<?php echo $year ?>" style="display: none;">
+                            <?php foreach($articles as $article){  ?>
+                            <li class="uk-flex uk-flex-space-between">
+                                <a href="<?php echo $article['canonical'].HTSUFFIX ?>"><?php echo $article['title'] ?></a>
+                                <div class="category-date"><?php echo date('d/m/y', strtotime($article['created_at'])) ?></div>
+                            </li>
+                            <?php } ?>
+                        </ul>
+                    </div>
+                    <?php 
+                        $firstYear = false;
+                        } 
+                    } else { 
+                    ?>
+                    <div class="category-item">
+                        <div class="uk-flex uk-flex-space-between">
+                            <span class="category-link">Chưa có bài viết</span>
                         </div>
                     </div>
+                    <?php } ?>
                 </div>
             </div>
 
@@ -118,3 +224,39 @@
         </div>
     </div>
 </section>
+
+<style>
+.submenu {
+    overflow: hidden;
+}
+
+.btn-more {
+    cursor: pointer;
+}
+
+.fa-chevron-down {
+    transition: transform 0.3s ease-in-out;
+}
+
+.fa-chevron-down.rotate{
+    transform: rotate(-180deg);
+}
+</style>
+
+<script>
+$(document).ready(function() {
+    // Handle toggle for both categories and years
+    $('.btn-more').on('click', function(e) {
+        e.preventDefault();
+        
+        var $caret = $(this).find('.fa-chevron-down');
+        var $dropdown = $(this).closest('.category-item').find('.submenu');
+        
+        // Toggle caret rotation
+        $caret.toggleClass('rotate');
+        
+        // Toggle submenu with smooth animation
+        $dropdown.slideToggle(300);
+    });
+});
+</script>
